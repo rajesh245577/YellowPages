13d7c519c9ee9722b8a4a47b725028dcec922cc4
rajesh245577 <rajeshannadurai24@gmail.com>
2026-09-19 00:17:13 +0530

    Fix WorkerProfile CSS import

--- Front-End/src/WorkerProfile/WorkerProfile.jsx
+++ Front-End/src/WorkerProfile/WorkerProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { data, useParams } from 'react-router-dom';
-import './WorkerProfile.css';
+import './Workerprofile.css';
 
 const WorkerProfile = () => {
     const { id } = useParams(); // email passed as :id
